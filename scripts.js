522d8b6ad781d2e258f9f772f2ff371d3a85b216
Łukasz Osmólski <93lukaszo93@gmail.com>
2017-04-26 22:05:08 +0200

    zadanie 9.2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-
+// zadanie 9.1 funkcje
 function getTriangleArea(a, h) {
     if ((a < 0) || (h < 0)) {
         return 'Nieprawidłowe dane';
@@ -15,3 +15,16 @@ var triangle1Area = getTriangleArea(5, 10);
 var triangle2Area = getTriangleArea(10, 20);
 var triangle3Area = getTriangleArea(3, 8);
 
+
+
+//zadanie 9.2 tablice
+var maleNames = ['Jan', 'Marian', 'Łukasz', 'Piotr', 'Maciek'];
+var femaleNames = ['Dorota', 'Małgorzata', 'Helena', 'Marianna', 'Ela'];
+
+var allNames = maleNames.concat(femaleNames);
+
+var newName ='Zdzich';
+
+if (allNames.indexOf(newName) === -1) {
+    allNames = allNames.push(newName);
+}
